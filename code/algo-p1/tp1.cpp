73ad7e21a056861ee5b271b15e002af032e6af8c
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2023-09-13 08:02:00 +0000

    modified:   code/algo-p1/tp1.cpp

--- code/algo-p1/tp1.cpp
+++ code/algo-p1/tp1.cpp
@@ -6,33 +6,45 @@ using namespace std;
 int tp1_4 () {
 	const int c_anglais = 2, c_maths = 5, c_info = 3;
 	
-	float anglais, maths, info;
+	float n_anglais, n_maths, n_info;
 	float moyenne;
 
-	cout << "Note en anglais (coeff. 2) : ";
-	cin >> anglais;
+	cout << "Note en anglais (coeff. "<< c_anglais <<") : ";
+	cin >> n_anglais;
 
-	cout << "Note en mathématiques (coeff. 5) : ";
-	cin >> maths;
+	cout << "Note en mathématiques (coeff. "<< c_maths <<") : ";
+	cin >> n_maths;
 
-	cout << "Note en informatiques (coeff. 3) : ";
-	cin >> info;
+	cout << "Note en informatiques (coeff. "<< c_info <<") : ";
+	cin >> n_info;
 
-	moyenne = (anglais * c_anglais + maths * c_maths + info * c_info) / (c_anglais + c_maths + c_info);
+	moyenne = (n_anglais * c_anglais + n_maths * c_maths + n_info * c_info) / (c_anglais + c_maths + c_info);
 
 	cout << "Moyenne obtenue : " << moyenne << endl;
 
 	return 0;
 }  
 
-int tp1_6 () {
+int tp1_61 () {
+	int v1, v2, v3, v4, temp;
 
+	cout << "Donnez 4 valeurs entières : ";
+	cin >> v1 >> v2 >> v3 >> v4;
+
+	temp = v1;
+
+	v1 = v2;
+	v2 = v3;
+	v3 = v4;
+	v4 = temp;
+
+	cout << "Après le décalage, v1 = " << v1 << ", v2 = " << v2 << ", v3 = " << v3 << ", v4 = " << v4 << endl;
 
 	return 0;
 }
 
 int main () {
-	tp1_6();
+	tp1_61();
 
 	return 0;
 }
