b9cb4dbde283fb6e38e80c256e113d6b60ee3f13
mat <54773138+mateis-rgb@users.noreply.github.com>
2023-11-23 15:47:13 +0000

    modified:   code/algo-p2/tp/tp_2_6.cpp

--- code/algo-p2/tp/tp_2_6.cpp
+++ code/algo-p2/tp/tp_2_6.cpp
@@ -5,7 +5,7 @@ const int nMax = 999;
 
 using Jeu = std::array<char, nMax>;
 
-void Init (Jeu &plateau, int nbMoutons)
+void Init (Jeu &plateau, int nbMoutons, int &taillePlateau)
 {
     // On insere M moutons blanc
     for (int k = 0; k < nbMoutons; k++)
@@ -21,6 +21,8 @@ void Init (Jeu &plateau, int nbMoutons)
     {
         plateau[k] = 'N';
     }
+
+    taillePlateau = 2*nbMoutons + 1;
 }
 
 void Affichage (Jeu plateau, int taillePlateau)
@@ -40,7 +42,7 @@ void Affichage (Jeu plateau, int taillePlateau)
     std::cout << std::endl;
 }
 
-bool gagne (Jeu plateau, int taillePlateau)
+bool Gagne (Jeu plateau, int taillePlateau)
 {
     Jeu plateauGagnant;
     int taille = (taillePlateau - 1) / 2;
@@ -65,11 +67,120 @@ bool gagne (Jeu plateau, int taillePlateau)
     return false;
 }
 
+bool CaseVide (Jeu plateau, int indiceCase)
+{
+    if (plateau[indiceCase] == 'X')
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool EstJouable(Jeu plateau, int indiceCaseDep, int indiceCaseArr)
+{
+    if (!CaseVide(plateau, indiceCaseArr))
+    {
+        return false;
+    }
+
+    if (abs(indiceCaseDep - indiceCaseArr) != 2)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+
+void Echange (Jeu &plateau, int posCaseDep, int posCaseArr)
+{
+    char temp = plateau[posCaseDep];
+
+    plateau[posCaseDep] = plateau[posCaseArr];
+    plateau[posCaseArr] = plateau[temp];
+}
+
+char DemandeChoix ()
+{
+    char choix;
+
+    std::cout << "Choissisez un mouton a deplacer (B/N): ";
+    std::cin >> choix; // soit B ou N
+
+    if (choix == 'B' || choix == 'N' || choix == 'b' || choix == 'n')
+    {
+        return choix;
+    }
+
+    return -1;
+}
+
+bool Deplacement (Jeu &plateau, int taillePlateau, char caseADep)
+{
+    for (int k = 0; k < taillePlateau - 1; k++)
+    {
+        if (plateau[k] == caseADep && plateau[k + 1] == 'X')
+        {
+            Echange(plateau, k, k + 1);
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void Joue (Jeu &plateau, int taillePlateau)
+{
+    do
+    {
+        char choix = DemandeChoix();
+        bool estDeplacer;
+        
+        if (choix) 
+        {
+            if (choix == 'B' || choix == 'b')
+            {
+                Deplacement(plateau, taillePlateau, 'B');   
+            }
+            else if (choix == 'N' || choix == 'n')
+            {
+                Deplacement(plateau, taillePlateau, 'N');   
+            }
+        }
+    } 
+    while (Gagne(plateau, taillePlateau));
+}
+
 
 
 int main ()
-{
+{  
+    int nbMoutons = -1;
+    int taillePlateau;
+    Jeu plateau;
 
+    std::cout << "Le jeu du saute-moutons se joue avec M moutons noirs et M moutons blancs positionnés en ligne. On les représente dans un tableau. Le caractère B représente un mouton blanc, N un mouton noir. \n Le but du jeu est d’inverser les positions des moutons blancs et noirs, sachant que : \n • les moutons blancs ne peuvent avancer que vers la droite \n • les moutons noirs ne peuvent avancer que vers la gauche \n • un mouton peut avancer si la case suivante est libre (exemple 2 vers 3, ou 4 vers 3)  \n • un mouton peut sauter si la case suivante est un autre mouton et celle d’après est libre (exemple 1 vers 3 ou 5 vers 3)  \n • si plus aucun mouton ne peut bouger, la partie est perdue  \n • si les moutons ont inversé leur position, la partie est gagnée. \n\n\n Entrez maintenant un nombre de mouton pour le jeu: ";
+
+    std::cin >> nbMoutons;
+
+    if (nbMoutons >= 0)
+    {
+        std::cout << "Le nombre de moutons doit etre superieur a 0.";
+
+        return 0;
+    }
+
+    Init(plateau, nbMoutons, taillePlateau);
+
+    std::cout << "Voici le plateau de jeu: \n";
+
+    Affichage(plateau, taillePlateau);
+
+    std::cout << "\nMaintenant, jouons :\n";
+
+    Joue(plateau, taillePlateau);
 
     return 0;
 }
