cc36d4e1a0d27a91fad21ceee5bb788c406c40a5
Mateis <mateis.pro@outlook.fr>
2023-12-14 09:04:11 +0100

    cours update

--- code/algo-p3/cm.cpp
+++ code/algo-p3/cm.cpp
@@ -20,9 +20,41 @@ int somme (int n)
 	return n + somme(n - 1);
 }
 
+int mystere (int n) // n >= 1
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	return mystere(n - 1) + 2 * (n - 1) + 1;
+}
+
+bool impaire (int);
+
+bool pair (int n) // n >= 0
+{
+	if (n == 0)
+	{
+		return true;
+	}
+
+	return impaire (n - 1);
+}
+
+bool impaire (int n) // n >= 0
+{
+	if (n == 1)
+	{
+		return true;
+	}
+	
+	return pair (n - 1);
+}
+
 int main()
 {
-	std::cout << factorielle(4);
+	
 
 	return 0;
 }
