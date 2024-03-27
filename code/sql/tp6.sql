867a81d10476f2f98709681a918fd444eec4f853
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-03-27 09:51:06 +0000

    new file:   code/sql/tp6.sql

---
+++ code/sql/tp6.sql
@@ -0,0 +1,8 @@
+SELECT nom || ' ' || prenom nom_prenom FROM musicien WHERE datnaiss < 1930;
+SELECT nom || ' ' || prenom nom_prenom FROM joue INNER JOIN album ON joue.refAlbum = album.refAlbum WHERE titre = 'Electric Ladyland';
+SELECT ville FROM label NATURAL JOIN album NATURAL JOIN joue WHERE nom = 'Coltrane' AND prenom = 'John';
+SELECT titre FROM album WHERE annee = (SELECT annee FROM album WHERE titre = 'Are You Experienced');
+SELECT MAX(duree) DureeMax FROM album;
+SELECT ville, COUNT(ville) FROM album NATURAL JOIN label GROUP BY ville;
+SELECT COUNT(refAlbum) albums FROM label NATURAL JOIN album NATURAL JOIN joue WHERE nom = 'Coltrane' AND prenom = 'John';
+SELECT nom || ' ' || prenom nom_prenom, COUNT(refAlbum) albums FROM label NATURAL JOIN album NATURAL JOIN joue WHERE nom != 'Coltrane' AND prenom != 'John';
