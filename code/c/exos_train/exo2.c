70a38a66ce5c735ac8ecf37319ce62b80a502633
Mateis <mateis.pro@outlook.fr>
2024-09-08 14:30:54 +0200

    Update exo2.c

--- code/c/exos_train/exo2.c
+++ code/c/exos_train/exo2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 
-time_t getCurrentTime ();
+time_t getCurrentTime (void);
 time_t getDifferenceTime (time_t start, time_t stop);
+int * getFormattedTime (time_t timestamp);
+void displayTime (int formattedTime[3]);
 
 int main (void)
 {
@@ -17,12 +19,13 @@ int main (void)
 
 	stopTime = getCurrentTime();
 
-	printf("votre temps de reflexion est estime a : %ld", getDifferenceTime(startTime, stopTime));
+	printf("vous avez mis ");
+	displayTime(getFormattedTime(getDifferenceTime(startTime, stopTime)));
 
 	return 0;
 }
 
-time_t getCurrentTime ()
+time_t getCurrentTime (void)
 {
 	return time(NULL);
 }
@@ -30,4 +33,23 @@ time_t getCurrentTime ()
 time_t getDifferenceTime (time_t start, time_t stop)
 {
 	return stop - start;
+}
+
+int * getFormattedTime (time_t timestamp)
+{
+	static int formattedTime[3];
+
+	formattedTime[0] = timestamp / 3600;
+	formattedTime[1] = (timestamp % 3600) / 60;
+	formattedTime[2] = timestamp % 60;
+
+	return formattedTime;
+}
+
+void displayTime (int formattedTime[4])
+{
+	if (formattedTime[0] != 0) printf("%dh", formattedTime[0]);
+	if (formattedTime[1] != 0) printf("%dm", formattedTime[1]);
+	
+	printf("%ds\n", formattedTime[2]);
 }
