6ab642bd024e2580398d06498cf54ad683437bbb
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-10-07 09:17:51 +0200

    Update dec2bin.c

--- cours/iut/but1/c/td4/dec2bin.c
+++ cours/iut/but1/c/td4/dec2bin.c
@@ -20,29 +20,44 @@ char * dec2bin (int n);
 
 int main (void)
 {
-    int dec = 19;
-    char * bin = dec2bin(dec);
+    // int dec = 19;
+    // char * bin = dec2bin(dec);
 
-    printf("%d en binaire donne %s", dec, bin);
+    // printf("%d en binaire donne %s", dec, bin);
+
+    char * a = "to";
+    char * b = "to";
+    char * cat = concat(a, b);
+
+
+    printf("%s + %s = %s", a, b, cat);
 
     return 0;
 }
 
-char * concat (char * a, char * b)
+char * concat (char * string1, char * string2)
 {
-    char * tempa = a;
-    char * tempb = b;
+    char * stringTemp = string1;
 
-    strcat(&tempa, &tempb);
+    int i = 0;
+    int j = strlen(stringTemp);
+    int str2len = strlen(string2);
 
-    return a;
+    for (i = 0; i < str2len; i++)
+    {
+        stringTemp[i+j] = string2[i];
+    }
+
+    stringTemp[i+j] = '\0';
+
+    return stringTemp;
 }
 
 
-char * dec2bin (int n)
-{
-    if (n == 0) return "0";
-    if (n == 1) return "1";
+// char * dec2bin (int n)
+// {
+//     if (n == 0) return "0";
+//     if (n == 1) return "1";
 
-    return concat(n % 2 == 0 ? "0" : "1", dec2bin(n/2));
-} 
+//     return concat(n % 2 == 0 ? "0" : "1", dec2bin(n/2));
+// } 
