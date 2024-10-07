e2f6c6f46972d3cfef3d6f186075281c3b605372
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-10-07 08:46:45 +0200

    update

--- cours/iut/but1/c/td4/dec2bin.c
+++ cours/iut/but1/c/td4/dec2bin.c
@@ -2,6 +2,15 @@
 #include <string.h>
 
 /**
+ * @brief Concatenate a + b strings
+ * 
+ * @param a 
+ * @param b 
+ * @return char* 
+ */
+char * concat (char * a, char * b);
+
+/**
  * @brief Convert dec number into binary
  * 
  * @param n 
@@ -19,12 +28,21 @@ int main (void)
     return 0;
 }
 
+char * concat (char * a, char * b)
+{
+    char * tempa = a;
+    char * tempb = b;
+
+    strcat(&tempa, &tempb);
+
+    return a;
+}
+
+
 char * dec2bin (int n)
 {
     if (n == 0) return "0";
     if (n == 1) return "1";
 
-    strcat()
-
-    return n % 2 == 0 ? "0" + dec2bin(n/2) : "1" + dec2bin(n/2);
+    return concat(n % 2 == 0 ? "0" : "1", dec2bin(n/2));
 } 
