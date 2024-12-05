61beba7afcd5c18c7b2266ae1c260f54cfa7f427
mat <54773138+mateis-rgb@users.noreply.github.com>
2024-12-05 08:05:02 +0100

    Update main.c

--- code/c/chess.c/main.c
+++ code/c/chess.c/main.c
@@ -8,11 +8,46 @@ typedef struct Piece
 	char color;
 } Piece;
 
+typedef struct Coordonne
+{
+	int pos_x;
+	int pos_y;
+} Coordonne;
+
+
+void displayGrid (Piece grid[8][8]);
+
+void generateGrid (Piece grid[8][8]);
+
+void initGrid (Piece grid[8][8]);
+
+int move (Piece grid[8][8], Coordonne from, Coordonne to);
+
+int canMove (Piece grid[8][8], Coordonne from, Coordonne to);
+
+void echange (Piece grid[8][8], Piece removed[32], int removed_size, Coordonne from, Coordonne to);
+
+
+int main (void)
+{ 
+	Piece grid[8][8];
+	
+	generateGrid(grid);
+	
+	displayGrid(grid);
+
+	initGrid(grid);
+
+	displayGrid(grid);
+
+	return 0;
+}
+
+
 void displayGrid (Piece grid[8][8])
 {
 	int i = 0;
 	int j = 0;
-	char toDisplay = '0';
 
 	printf("+---+---+---+---+---+---+---+---+---+\n");
 	printf("|   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |\n");
@@ -58,8 +93,22 @@ void generateGrid (Piece grid[8][8])
 
 void initGrid (Piece grid[8][8])
 {
+	/**
+	 * 1 = p = pion
+	 * 2 = c = cavalier
+	 * 3 = t = tour
+	 * 4 = f = fou
+	 * 5 = d = dame
+	 * 6 = r = roi
+	*/
 	int k = 0;
 
+	int placementB[8] = {3, 2, 4, 5, 6, 4, 2, 3};
+	int placementN[8] = {3, 2, 4, 6, 5, 4, 2, 3};
+
+	char * toName;
+
+	// Placement des pions
 	for (k = 0; k < 8; k++)
 	{
 		Piece pionB = {1, "pion", 0};
@@ -68,21 +117,76 @@ void initGrid (Piece grid[8][8])
 		grid[6][k] = pionN;
 	}
 
-	
+	// Placement des blancs
+	for (k = 0; k < 8; k++)
+	{
+		if (placementB[k] == 1) toName = "pion";
+		else if (placementB[k] == 2) toName = "cavalier";
+		else if (placementB[k] == 3) toName = "tour";
+		else if (placementB[k] == 4) toName = "fou";
+		else if (placementB[k] == 5) toName = "dame";
+		else if (placementB[k] == 6) toName = "roi";
+
+		grid[0][k] = (Piece) {placementB[k], toName, 0};
+	}
+
+	// Placement des blancs
+	for (k = 0; k < 8; k++)
+	{
+		if (placementN[k] == 1) toName = "pion";
+		else if (placementN[k] == 2) toName = "cavalier";
+		else if (placementN[k] == 3) toName = "tour";
+		else if (placementN[k] == 4) toName = "fou";
+		else if (placementN[k] == 5) toName = "dame";
+		else if (placementN[k] == 6) toName = "roi";
+
+		grid[7][k] = (Piece) {placementN[k], toName, 1};
+	}
 }
 
 
-int main (void)
-{ 
-	Piece grid[8][8];
-	
-	generateGrid(grid);
-	
-	displayGrid(grid);
+int move (Piece grid[8][8], Coordonne from, Coordonne to)
+{
+	return 0;
+}
 
-	initGrid(grid);
+int canMove (Piece grid[8][8], Coordonne from, Coordonne to)
+{
+	if (grid[from.pos_x][from.pos_y].id == 1)
+	{
 
-	displayGrid(grid);
+	}
+
+	else if (grid[from.pos_x][from.pos_y].id == 2)
+	{
+
+	}
+
+	else if (grid[from.pos_x][from.pos_y].id == 3)
+	{
+
+	}
+
+	else if (grid[from.pos_x][from.pos_y].id == 4)
+	{
+
+	}
+
+	else if (grid[from.pos_x][from.pos_y].id == 5)
+	{
+
+	}
+
+	else if (grid[from.pos_x][from.pos_y].id == 6)
+	{
+
+	}
+
 
 	return 0;
+}
+
+void echange (Piece grid[8][8], Piece removed[32], int removed_size, Coordonne from, Coordonne to)
+{
+
 }
